ea3c199cb73faae7c0a40e9df6ace214e1d4e3fd
Fabian Andiel <fabian.andiel@gmail.com>
2024-08-14 20:27:18 +0200

    sets up project and post method

---
+++ src/Controller/MainController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Person;
+
+class MainController extends AbstractController
+{
+    #[Route('/main', name: 'app_main')]
+    public function index(): JsonResponse
+    {
+        
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MainController.php',
+        ]);
+    }
+
+    #[Route('/add', name: 'add_person')]
+    public function add_person(Request $request, ManagerRegistry $doctrine ): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+        $content = $request->getContent();
+        $content = json_decode($content, true);
+        $person=$this->createPersonFromRequest($content);
+        $entityManager->persist($person);
+        $entityManager->flush();
+ 
+        return $this->json([
+            'message' => 'Content given out '.$person->getFirstname(),
+            'path' => 'src/Controller/MainController.php',
+        ]);
+    }
+
+
+    private function createPersonFromRequest($requestContent): Person {
+        $person = new Person();
+        $person->setFirstname($requestContent["firstname"]);
+        $person->setLastname($requestContent["lastname"]);
+        $person->setStreet($requestContent["street"]);
+        $person->setHousenumber($requestContent["housenumber"]);
+        $person->setAppartment($requestContent["appartment"]);
+        $person->setPhonenumber($requestContent["phonenumber"]);
+
+        return $person;
+    }
+
+
+}
---
+++ src/Entity/Person.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PersonRepository::class)]
+class Person
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $street = null;
+
+    #[ORM\Column]
+    private ?int $housenumber = null;
+
+    #[ORM\Column]
+    private ?int $appartment = null;
+
+    #[ORM\Column]
+    private ?int $phonenumber = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): static
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getHousenumber(): ?int
+    {
+        return $this->housenumber;
+    }
+
+    public function setHousenumber(int $housenumber): static
+    {
+        $this->housenumber = $housenumber;
+
+        return $this;
+    }
+
+    public function getAppartment(): ?int
+    {
+        return $this->appartment;
+    }
+
+    public function setAppartment(int $appartment): static
+    {
+        $this->appartment = $appartment;
+
+        return $this;
+    }
+
+    public function getPhonenumber(): ?int
+    {
+        return $this->phonenumber;
+    }
+
+    public function setPhonenumber(int $phonenumber): static
+    {
+        $this->phonenumber = $phonenumber;
+
+        return $this;
+    }
+}
